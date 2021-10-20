532bfa0333dcc81c50134ffa7cf238f3e7e8320e
wagnerkaba <bom.link+github@gmail.com>
2021-10-20 15:27:42 -0300

    Criação do Spring Boot

---
+++ src/main/java/testewagner/citiesapi/CitiesapiApplication.java
@@ -0,0 +1,13 @@
+package testewagner.citiesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitiesapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitiesapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/testewagner/citiesapi/CitiesapiApplicationTests.java
@@ -0,0 +1,13 @@
+package testewagner.citiesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitiesapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
