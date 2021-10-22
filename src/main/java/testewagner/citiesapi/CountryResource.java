68ed314adf773dd295013525c537543f552f58bf
wagnerkaba <bom.link+github@gmail.com>
2021-10-22 15:01:58 -0300

    Implementação da método GET em /countries/page com parâmetro Pageable

--- src/main/java/testewagner/citiesapi/CountryResource.java
+++ src/main/java/testewagner/citiesapi/CountryResource.java
@@ -1,6 +1,8 @@
 package testewagner.citiesapi;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,5 +26,10 @@ public class CountryResource {
     public List<Country> countries(){
         return repository.findAll();
 
+    }
+    @GetMapping("/page")
+    //Experimente chamar GET neste endereço: http://localhost:8080/countries/page?page=0&size=10&sort=name,desc
+    public Page<Country> countriesPage(Pageable page){
+        return repository.findAll(page);
     }
 }
